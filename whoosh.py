958afddf8890f9e4cfbe807cebf2f976d04a31eb
Maxim Oransky <maxim.oransky@gmail.com>
2021-11-03 22:36:17 +0300

    whoosh.py

--- whoosh.py
+++ whoosh.py
@@ -1,12 +1,50 @@
+import json
 import os
-import PIL
+
+from PIL import Image
+from pilkit.processors import SmartResize
+from pilkit.utils import process_image
 
 
-def main(root):
-    os.makedirs('i')
-    for i, image in enumerate(os.listdir(root)):
-        print(i, image)
+def main(root, dest, sizes):
+    options = {
+        'progressive': True,
+        'optimize': True,
+        'quality': 85,
+    }
+
+    for size in sizes:
+        os.makedirs(f'{dest}/{size}x{size}', exist_ok=True)
+
+    files = os.listdir(root)
+    for i, name in enumerate(files):
+        print(i, name)
+        img = Image.open(f'{root}/{name}')
+
+        for size in sizes:
+            icon_path = f'{dest}/{size}x{size}/{i}.jpg'
+            print('-->', icon_path)
+
+            if os.path.exists(icon_path):
+                os.remove(icon_path)
+
+            icon = process_image(img, processors=[SmartResize(size, size)], options=options)
+            with open(icon_path, 'wb') as f:
+                f.write(icon.getvalue())
+
+    index = {
+        'sizes': sizes,
+        'max': len(files),
+    }
+
+    with open(f'{dest}/index.json', 'w') as f:
+        json.dump(index, f)
 
 
 if __name__ == '__main__':
-    main('source')
+    main('source', 'i', sizes=[
+        80,
+        120,
+        160,
+        240,
+    ])
